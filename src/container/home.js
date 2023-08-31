118f4d489adf2b0098dbbb5afe39bc7de56302b7
Syed Ziauddin <syed.nlp00@gmail.com>
2023-08-31 16:43:55 +0530

    Update home.js

--- src/container/home.js
+++ src/container/home.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import Card from '../components/Card';
+import React,{ Suspense } from 'react'
+const Card = React.lazy(() => import('../components/Card'));
 
 const Home = () => {
   return (
     <>
+    <Suspense fallback={<p style={{textAlign:'center'}}>
+    Please Wait, Let me cooking something for You.
+    </p>}>
     <Card/>
+    </Suspense>
     <p style={{textAlign:'center',marginTop:'2em',padding:'1em'}}>
       More are coming soon...
     </p>
